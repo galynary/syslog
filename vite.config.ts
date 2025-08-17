0d0d572efa27d823780004288691eac600ee26d8
galynary <galyna.rybak.789@gmail.com>
2025-08-17 19:27:40 +0300

    update project

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/syslog/", // 👈 назва репозиторію на GitHub
+});
